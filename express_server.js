fca27b62b2f4c5f7f4420e03e26e51301a2ad363
Manuel Casanova <manucasanova@hotmail.com>
2022-03-02 21:16:24 -0800

    MODIFY ROUTES TO DISPLAY USERNAME

--- express_server.js
+++ express_server.js
@@ -50,17 +50,26 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render("urls_index", templateVars);
 }); //
 
 app.get("/urls/:shortURL", (req, res) => {
-  let shortURL = req.params.shortURL; //Had to declare this variable to call it below. Otherwise I could not get any information.
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL] };
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"],
+  };
   res.render("urls_show", templateVars);
 }); 
 
