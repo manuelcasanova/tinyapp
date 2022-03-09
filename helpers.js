59229fb41f25e348cf2cbc4928656143b7112e92
Manuel Casanova <manucasanova@hotmail.com>
2022-03-09 15:32:54 -0800

    MOVED ALL FUNCTIONS FROM EXPRESS SERVER

--- helpers.js
+++ helpers.js
@@ -6,6 +6,39 @@ function getUserByEmail(email, userDatabase) { //Returns the ID for the user wit
   }
 };
 
+function generateRandomString() {
+  let result = '';
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const length = 6;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+    //Math.random --> 0...1  times (length of characters) charAt --> gets the character of the string
+  }
+  return result;
+}
+
+function emailExists(email, userDatabase) { //Checks if the email corresponds to a user in the database
+  for (const user in userDatabase) {
+    if (userDatabase[user].email === email) {
+      return true;
+    }
+  }
+  return false;
+};
+
+function urlsForUser(id, urlDatabase) { //Function returns the URLS where the userID is equal to the id of the logged-in user
+  const userURLS = {};
+  for (let k in urlDatabase) {
+    if (urlDatabase[k].userID === id) {
+    userURLS[k] = urlDatabase[k];
+    }
+  }
+  return userURLS;
+}
+
 module.exports = {
-  getUserByEmail
+  getUserByEmail,
+  generateRandomString,
+  emailExists,
+  urlsForUser
 };
